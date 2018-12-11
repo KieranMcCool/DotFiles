179640a1386577eefba8ac603208159d9517a08e
Kieran McCool <kieranmccool332@gmail.com>
2018-12-11 23:03:12 +0000

    shut the install script up and made it so it run from current working directory

--- install.sh
+++ install.sh
@@ -6,27 +6,22 @@
 
 ########## Variables
 
-dir=~/DotFiles                    # dotfiles directory
-olddir=~/DotFiles_old             # old dotfiles backup directory
-files=$(find . -not -path "./.git*" -not -name ".gitignore" -not -name ".git*" -not -name "install.sh" -type f -exec basename {} \; | xargs echo)
+dir=$(pwd)                   # dotfiles directory
+olddir=$(pwd)/backup # old dotfiles backup directory
+files=$(find . -not -path "./backup" -not -path "./.git*" -not -name ".gitignore" \
+    -not -name ".git*" -not -name "install.sh" -type f -exec basename {} \; | xargs echo)
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p "$olddir"
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file $olddir
-    echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
 
