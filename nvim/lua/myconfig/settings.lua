b687fdb3e5250fe243936d89535d314e3c6463b2
Kieran McCool <KieranMcCool332@gmail.com>
2025-09-03 23:46:42 +0100

    vim tab bar improvements

--- nvim/lua/myconfig/settings.lua
+++ nvim/lua/myconfig/settings.lua
@@ -7,7 +7,46 @@ vim.opt.termguicolors = true
 vim.opt.mouse = "a"
 
 -- Enable bufferline (tab bar)
-require("bufferline").setup{}
+require("bufferline").setup{
+  options = {
+    mode = "buffers",
+    themable = true,
+    numbers = "none",
+    close_command = "bdelete! %d",
+    right_mouse_command = "bdelete! %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = nil,
+    indicator = {
+      icon = '▎',
+      style = 'icon',
+    },
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    max_name_length = 18,
+    max_prefix_length = 15,
+    tab_size = 18,
+    diagnostics = false,
+    show_buffer_icons = true,
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true,
+    separator_style = "slant",
+    enforce_regular_tabs = false,
+    always_show_bufferline = true,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        text_align = "center",
+        separator = true
+      }
+    }
+  }
+}
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
